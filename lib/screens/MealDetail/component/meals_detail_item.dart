8babf53f3fb83f52b0a3cdb54e70a4f61deaf8cc
usama <codingeyes6@gmail.com>
2022-05-23 20:05:35 +0500

    meal detail screen complete

--- lib/screens/MealDetail/component/meals_detail_item.dart
+++ lib/screens/MealDetail/component/meals_detail_item.dart
@@ -4,22 +4,25 @@ import '../../../styles/textstyles.dart';
 import '../../../models/meals.dart';
 
 Widget mealsDetailItem(Meal meal) {
-  return Column(
-    children: [
-      mealImage(meal.imageUrl),
-      const SizedBox(
-        height: 10,
-      ),
-      buildSectionTitle(meal.title),
-      ingrediantsList(meal)
-    ],
+  return SingleChildScrollView(
+    child: Column(
+      children: [
+        mealImage(meal.imageUrl),
+        const SizedBox(height: 10),
+        buildSectionTitle(meal.title),
+        ingrediantsList(meal),
+        const SizedBox(height: 5),
+        buildSectionTitle("Steps"),
+        stepsList(meal)
+      ],
+    ),
   );
 }
 
 Widget mealImage(String imageUrl) {
   return SizedBox(
     width: double.infinity,
-    height: 150,
+    height: 200,
     child: Image.network(
       imageUrl,
       fit: BoxFit.cover,
@@ -27,10 +30,8 @@ Widget mealImage(String imageUrl) {
   );
 }
 
-
-
-Widget buildSectionTitle(String title){
-return Padding(
+Widget buildSectionTitle(String title) {
+  return Padding(
     padding: const EdgeInsets.only(bottom: 10),
     child: Text(
       title,
@@ -40,11 +41,7 @@ return Padding(
 }
 
 Widget ingrediantsList(Meal m) {
-  return Container(
-    width: 300,
-    height: 170,
-    padding: const EdgeInsets.symmetric(vertical: 10,horizontal: 15),
-    decoration: ingrediantBox(),
+  return buildContainer(
     child: ListView.builder(
       itemBuilder: (ctx, index) {
         return ingrediantItem(m.ingredients[index]);
@@ -54,23 +51,53 @@ Widget ingrediantsList(Meal m) {
   );
 }
 
-
-BoxDecoration ingrediantBox(){
+BoxDecoration ingrediantBox() {
   return BoxDecoration(
-    color: Colors.white,
-    border: Border.all(color: Colors.grey),
-    borderRadius: BorderRadius.circular(10)
+      color: Colors.white,
+      border: Border.all(color: Colors.grey),
+      borderRadius: BorderRadius.circular(10));
+}
+
+Widget buildContainer({Widget? child}) {
+  return Container(
+    width: 300,
+    height: 180,
+    margin: const EdgeInsets.all(10),
+    padding: const EdgeInsets.symmetric(vertical: 10, horizontal: 15),
+    decoration: ingrediantBox(),
+    child: child,
   );
 }
 
-Widget ingrediantItem(String ingrediants){
+Widget ingrediantItem(String ingrediants) {
+  return Card(
+    color: accentColor,
+    child: Padding(
+      padding: const EdgeInsets.all(8.0),
+      child: Text(ingrediants),
+    ),
+  );
+}
 
-return Card(
-  color: accentColor,
-  child: Padding(
-    padding: const EdgeInsets.all(8.0),
-    child: Text(ingrediants),
-  ),
-);
+Widget stepsList(Meal m) {
+  return buildContainer(
+    child: ListView.builder(
+      itemBuilder: (ctx, index) {
+        return stepItem(index + 1,m.steps[index]);
+      },
+      itemCount: m.steps.length,
+    ),
+  );
+}
 
+Widget stepItem(int index,String steps) {
+  return Column(
+    children: [
+      ListTile(
+        leading: CircleAvatar(child: Text('# $index')),
+        title: Text(steps),
+      ),
+      const Divider(thickness: 2,)
+    ],
+  );
 }
